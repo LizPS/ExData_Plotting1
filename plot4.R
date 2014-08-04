e438f822510fdaafd0c05a42d132e448cf20d0d7
Liz Supinski <liz@supinski.net>
2014-08-04 19:25:50 -0400

    fix typo propagated through comments of all four files

--- plot4.R
+++ plot4.R
@@ -1,12 +1,14 @@
-##Read in and subset data (to save time, if objects are already in memory already begin on
-## line ) This script assumes data are unzipped and in working directory.
+##This script assumes data are unzipped and in working directory.
+
+#Read in and subset data (to save processing time, if objects are already in 
+#memory execute from line 15) 
 col_classes <- c(rep("character", 2), rep("numeric", 7))
 data <- read.table("household_power_consumption.txt", sep=";", na.strings ="?",
         nrows = 2075259, stringsAsFactors = FALSE, header = TRUE,
         colClasses = col_classes)
 data$datetime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
-#Subset on the days of interest and overwrite data to reduce memory use
+#overwrite data in memory with subset to reduce memory use
 begin <- as.Date("2007-02-01")
 end <- as.Date("2007-02-02")
 data <- subset(data, Date %in% c(begin, end))
